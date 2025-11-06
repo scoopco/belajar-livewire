885648a2d34e9a9d7d2ccfd9cc76c4da466a2964
faqihyugos <faqihyugos@gmail.com>
2025-11-06 14:41:04 +0700

    first commit livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\Users;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/counter', Counter::class);
+Route::get('/user', function () {
+    return view('users');
 });
